ffee6a7a793f51e635424cea82d8575c68d925d8
Jason Perez <jp3476@columbia.edu>
2017-09-27 11:56:03 -0400

    beam py

--- python/gopigo/hw1_bdb2130_cjh2209_jp3476/beam_width.py
+++ python/gopigo/hw1_bdb2130_cjh2209_jp3476/beam_width.py
@@ -6,17 +6,36 @@ from time import sleep
 Set up the GoPiGo so that its sensor is less than 60 centimeters away from a single object
 at a direct 90 degree angle left of the sensor.'''
 
-servo_pos=78
+# WILL WORK ONLY FOR VERY THIN OBJECTS IE. A BROOM OR LONG THIN CYLINDER
+
+servo_pos=180
 
 servo(servo_pos)
 sleep(1)
 
 while True:
     servo(servo_pos)
-    servo_pos += 1
+    # continuously increments servo angle
+	servo_pos += 1
     sleep(.5)
     dist = us_dist(15)
-    print("distance: " + str(dist))
-    if(dist < 60):
-        print('Beam width: ' + str(2 * (180 - servo_pos)))
-        break
+
+	# search, detect object, once detected - record the servo angle
+	# keep sensor servo moving until object is no longer in view
+	# compute then store measured beam width
+
+	objdetect = 0
+	objnoshow = 0
+    
+	print("distance: " + str(dist))
+    
+	if(dist < 60):
+        #print('Beam width: ' + str(2 * (180 - servo_pos)))
+        #break
+		objdetect = servo_pos
+	
+	if(dist > 60):
+		objnoshow = servo_pos
+		beamwidth = objdetect - objnoshow
+		print(str(beamwidth));
+		break;
