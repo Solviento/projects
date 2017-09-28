6755a3494525494c7078d76bf9b82a8f27df4c36
Jason Perez <jp3476@columbia.edu>
2017-09-28 01:20:26 -0400

    submission

--- python/gopigo/hw1_bdb2130_cjh2209_jp3476/beam_width.py
+++ python/gopigo/hw1_bdb2130_cjh2209_jp3476/beam_width.py
@@ -1,41 +1,52 @@
 from gopigo import *
 from time import sleep
 
-'''Calculates the beam width.
+'''Calculates and prints out the beam width.
 
-Set up the GoPiGo so that its sensor is less than 60 centimeters away from a single object
-at a direct 90 degree angle left of the sensor.'''
+Set GoPiGo down facing roughly toward the Ben's water 
+bottle set 100cm away'''
 
-# WILL WORK ONLY FOR VERY THIN OBJECTS IE. A BROOM OR LONG THIN CYLINDER
-
-servo_pos=180
+servo_pos=0
 
 servo(servo_pos)
 sleep(1)
 
-while True:
-    servo(servo_pos)
-    # continuously increments servo angle
-	servo_pos += 1
-    sleep(.5)
-    dist = us_dist(15)
+has_seen = False
+has_seen_twice = False
+ct = 0
 
-	# search, detect object, once detected - record the servo angle
+objdetect = 0
+objnoshow = 0
+
+while True:
+	
+	# search, detect object, once detected - recor the servo angle
 	# keep sensor servo moving until object is no longer in view
 	# compute then store measured beam width
-
-	objdetect = 0
-	objnoshow = 0
-    
-	print("distance: " + str(dist))
-    
-	if(dist < 60):
-        #print('Beam width: ' + str(2 * (180 - servo_pos)))
-        #break
-		objdetect = servo_pos
 	
-	if(dist > 60):
+    servo(servo_pos)
+    
+    # continuously increment servo angle
+    servo_pos += 1
+    sleep(.5)
+    dist = us_dist(15)
+    
+    print("distance: " + str(dist))
+    print('servo pos ' + str(servo_pos))
+    
+    if (dist <= 120 and not has_seen):
+		has_seen = True
+		objdetect = servo_pos
+		ct += 1
+		
+    elif(dist <= 120 and has_seen):
+		ct += 1
+		
+		
+    elif (dist > 120 and has_seen and ct > 3):
 		objnoshow = servo_pos
-		beamwidth = objdetect - objnoshow
-		print(str(beamwidth));
-		break;
+		beamwidth = objnoshow - objdetect
+		
+		# Subtract 3 for angle of bottle (asin(6cm / 100cm) = 3.4 deg)
+		print('beam width ' + str(beamwidth - 3))
+		break
--- python/gopigo/hw1_bdb2130_cjh2209_jp3476/locate_object.py
+++ python/gopigo/hw1_bdb2130_cjh2209_jp3476/locate_object.py
@@ -4,122 +4,87 @@ import math
 from gopigo import *
 from time import sleep
 
-# adjust sonic sensor servo with added offset bias (vals: 0-90-180)
-servo_pos = 78
+'''Locates and approaches an object 1 meter behind the GoPiGo'''
+
+# adjust sonic sensor servor with added offset bias (vals: 0-90-180)
+servo_pos = 80
 servo(servo_pos)
 
 #adjust motor speed (no units)
 set_speed(50)
 
-# Turn until object seen
-while True:
-	# sets wheel encoder, third parameter = pulses (32 pulses ~ 1 wheel rotation)
-    enc_tgt(1, 0, 1)
-    # stores ultrasonic sensor current recorded distance
-    dist = us_dist(15)
-    set_speed(50)
-    # rotate left continuously using an encoder target
-    left_rot()
-    # print ultrasonic distance for debugging purposes
-    print(dist)
-    # sleep for one second so that robot has time to carry out commands
-    sleep(1)
-    # sensor has located object within 120 cm (adjusted for 1m)
-    if(dist < 120):
-        break
-    
-sleep(1)
-print('found obj!')
-# we keep track of pulse count to know how many pulses the object
-# requires to fully cover using the sensor's conical beam
-pulse_count = 0
 
-# Keep turning until object no longer seen
-while True:
-	enc_tgt(1, 0, 1)
-    left_rot()
-    
-    pulse_count += 1
-    dist = us_dist(15)
-    print('Pulses: ' + str(pulse_count * 1)) 
-    print(dist)
-    # once object is no longer in view of sonic sensor, we turn back
-    if (dist > 120):
-        break
-    sleep(1)
-    
-print('Turning back...')
-sleep(1)
-# will rotate using specific number of pulses based on how wide the object is
-enc_tgt(1, 0, 1+int(math.ceil(float(pulse_count)/2)))
-right_rot()
+def search_and_dest(to_distance, final):
 
-sleep(1)
+	# Turn until object seen
+	while True:
+		# sets wheel encoder, third parameter = pulses (32 pulses ~ 1 wheel rotation)
+		enc_tgt(0, 1, 1)
+		# rotate left continuously using an encoder target
+		left()
+		# sleep for one second so that robot has time to carry out commands
+		sleep(1)
+		# stores ultrasonic sensor current recorded distance
+		dist = us_dist(15)
+		# print ultrasonic distance for debugging purposes
+		print(dist)
+		
+		# sensor has located object within 120 cm (adjusted for 1m)
+		if(dist < 140):
+			break
+		
+	sleep(1)
+	print('found obj!')
+	# we keep track of pulse count to know how many pulses the object
+	# requires to fully cover using the sensor's conical beam
+	pulse_count = 0
 
-# move forward in 1 pulse rotations until sensor confirms we are 55 cm away
-while True:
-    dist = us_dist(15)
-    enc_tgt(1, 0, 1)
-    forward()
-    print(dist)
-	# after many runs, we see that the robot is guaranteed to veer away
-	# from the object, therefore we reset the robot's local frame
-    if (dist < 55):
-        print(dist)
-        stop()
-        break
-    sleep(1)
+	# Keep turning until object no longer seen
+	while True:
+		enc_tgt(0, 1, 1)
+		left()
+		sleep(1)
+		
+		pulse_count += 1
+		dist = us_dist(15)
+		print('Pulses: ' + str(pulse_count * 1)) 
+		print(dist)
+		# once object is no longer in view of sonic sensor, we turn back
+		if (dist > 140):
+			break
+		
+	print('Turning back...')
+	sleep(1)
+	# will rotate using specific number of pulses based on how wide the object is
+	enc_tgt(1, 0, int(math.ceil(float(pulse_count)/2)))
+	right()
 
-sleep(1)
+	sleep(1)
 
-# reconfigure encoder target so that robot's sensor is reset once we rotate
-# in order to locate the object again
-enc_tgt(1, 0, 5)
-right_rot()
+	# move forward in 1 pulse rotations until sensor confirms we are 55 cm away
+	while True:
+		dist = us_dist(15)
+		enc_tgt(1, 0, 1)
+		forward()
+		print(dist)
+		# after many runs, we see that the robot is guaranteed to veer away
+		# from the object, therefore we reset the robot's local frame
+		if (dist < to_distance):
+			print(dist)
+			stop()
+			break
+		sleep(1)
 
-sleep(1)
+	sleep(1)
+		
+	if not final:
+		# reconfigure encoder target so that robot's sensor is reset once we rotate
+		# in order to locate the object again
+		enc_tgt(1, 0, 5)
+		right_rot()
+		sleep(1)
 
-# Turn until object seen
-while True:
-    enc_tgt(1, 0, 1)
-    dist = us_dist(15)
-    set_speed(50)
-    left_rot()
-    print(dist)
-    sleep(1)
-    if(dist < 80):
-        break
-
-sleep(1)
-print('found obj!')
-pulse_count = 0
-
-while True:
-    enc_tgt(1, 0, 1)
-    left_rot()
-    pulse_count += 1
-    dist = us_dist(15)
-    print('Pulses: ' + str(pulse_count * 1)) 
-    print(dist)
-    # sensor is out of range with object, turn back
-    if (dist > 80):
-        break
-    sleep(1)
-    
-print('Turning back...')
-sleep(1)
-# adding +1 to take into account the right side bias
-enc_tgt(1, 0, 1+int(math.ceil(float(pulse_count)/2)))
-right_rot()
-
-sleep(1)
-
-# move forward until robot is 20cm away from object
-while True:
-    dist = us_dist(15)
-    enc_tgt(1, 0, 1)
-    forward()
-    if(dist<20):
-        stop()
-        break
+search_and_dest(80, False)
+search_and_dest(50, False)
+search_and_dest(23, True) # Note that the distance sensor reads 20cm as 23
     
